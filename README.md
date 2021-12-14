dd61c04ecaaa9fff8eb8ff0fe16abe09d91327db
Rainly0036 <78688985+Rainly0036@users.noreply.github.com>
2021-12-14 10:33:17 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+## Homework Correction Tracking System
+This is a system that can track your homework correction time & date 
+
+### Warning 
+This setup is **extremely hard** for person that do not have *any* experience in Discord.js library or hosting bot. 
+
+### Requirement
+- A Discord Bot (Recommended Video: [Make A Discord Bot](https://www.youtube.com/watch?v=_woxAkO0Ywg))
+
+- Your bot can online 24/7 (Recommended Platform: [Heroku](https://dashboard.heroku.com/))
+- Common sense 
+- A server that you owned (We need a channel later)
+- Node.js Version 17 or latest
+- Some experience in discord.js 
+
+Make sure you done all of it.
+
+### Start
+
+1. Copy your bot token from [Discord Developer Portal](https://discord.com/developers/applications)
+2. Inside `/storage/config.json` file, paste it in "PASTE-YOUR-BOT-TOKEN-HERE"
+![image info](./assets/botToken.jpg)
---
+++ index.js
@@ -0,0 +1,45 @@
+const Discord, { MessageEmbed } = require('discord.js')
+const client = new Discord.Client({
+    partials: ["MESSAGE","CHANNEL","USER"],
+    intents: [
+        Discord.Intents.FLAGS.GUILDS,
+        Discord.Intents.FLAGS.GUILD_MEMBERS,
+        Discord.Intents.FLAGS.GUILD_MESSAGES,
+        Discord.Intents.FLAGS.DIRECT_MESSAGES,
+        Discord.Intents.FLAGS.GUILD_VOICE_STATES,
+    ]
+})
+
+client.on("messageUpdate", (oldMessage, newMessage) => {
+    if (newMessage.member.user.bot) return;
+    
+    // CHANGE YOUR CHANNEL ID HERE
+    if (newMessage.channelId === "YOUR-CHANNEL-ID") {
+
+        var timeNow = Math.floor(new Date().getTime()/1000.0)
+
+        // AND HERE TOO
+        const channel = client.channels.cache.find(c => c.id === 'YOUR-CHANNEL-ID' && c.type === 'GUILD_TEXT');
+        
+        // MESSAGE THAT THE BOT ALREADY SENDED IN THAT CHANNEL
+        channel.messages.fetch('BOT-MESSAGE-ID').then(msg => {
+            
+            let embed = new MessageEmbed()
+            .setDescription(`
+            **Homework Tracked since <t:${timeNow}:R>**
+            `)
+            .setFooter("Credit: Rainly36")
+            .setColor("#6ab59a")
+
+            msg.edit({
+                content: null,
+                embeds: [embed]
+            })
+
+        }).catch(err => {
+            console.error(err);
+        });
+    }
+})
+
+client.login(env.botToken)
